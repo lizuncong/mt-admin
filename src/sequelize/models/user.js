420db8a14cef7634d4e5bd96ec839fb2e00acc77
lizuncong <2900170896@qq.com>
2020-04-11 17:31:41 +0800

    stash

--- src/sequelize/models/user.js
+++ src/sequelize/models/user.js
@@ -1,23 +1,23 @@
-const Sequelize = require('sequelize')
+const { DataTypes } = require('sequelize');
 const seq = require('../index')
 
 
 const User = seq.define('user', {
   // id会自动创建，并设为主键，而且自增
   userName: {
-    type: Sequelize.STRING,
+    type: DataTypes.STRING,
     allowNull: false
   },
   password: {
-    type: Sequelize.STRING,
+    type: DataTypes.STRING,
     allowNull: false
   },
   nickname: {
-    type: Sequelize.STRING,
+    type: DataTypes.STRING,
     allowNull: false
   },
   status: {
-    type: Sequelize.INTEGER,
+    type: DataTypes.INTEGER,
     allowNull: false
   },
 })
