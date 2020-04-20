1503590340c2780f0897a46efc3e228a043a0cea
lizuncong <2900170896@qq.com>
2020-04-20 18:35:02 +0800

    引入ajv做json规则校验

---
+++ src/json-schema/index.js
@@ -0,0 +1,22 @@
+import Ajv from 'ajv'
+import userSchema from './user'
+/*
+* JSON schema 校验
+* */
+
+const ajv = new Ajv({
+  allErrors: true
+})
+
+const validate = (schema, data = {}) => {
+  const valid = ajv.validate(schema, data)
+  if (!valid) {
+    return ajv.errors
+  }
+}
+
+export default {
+  userValidate (data) {
+    return validate(userSchema, data)
+  }
+}
---
+++ src/json-schema/user.js
@@ -0,0 +1,36 @@
+/*
+* user数据格式规则
+* */
+
+const schema = {
+  type: 'object',
+  properties: {
+    userName: {
+      type: 'string',
+      // pattern: '^[a-zA-Z][a-zA-Z0-9_]+$',
+      maxLength: 255,
+      minLength: 2
+    },
+    password: {
+      type: 'string',
+      maxLength: 255,
+      minLength: 6
+    },
+    phone: {
+      type: 'string',
+      maxLength: 20,
+      minLength: 5
+    },
+    gender: {
+      type: 'number',
+      minimum: 1,
+      maximum: 3
+    },
+    avatar: {
+      type: 'string',
+      maxLength: 255
+    }
+  }
+}
+
+export default schema
